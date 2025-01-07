504d87edaf59fab99f5f0272af19171a2bb1f337
Ted Bouskill <618496+tedbouskill@users.noreply.github.com>
2025-01-07 00:13:27 -0600

    Added new functions

--- repo_packages/cls_string_helpers.py
+++ repo_packages/cls_string_helpers.py
@@ -1,8 +1,28 @@
+import re
+
 class StringHelpers:
     @staticmethod
-    def is_null_or_empty(string):
+    def is_null_or_empty(string: str) -> bool:
         return string is None or string == ''
 
     @staticmethod
-    def contains_alphabetic(text):
+    def contains_alphabetic(text: str) -> bool:
         return any(char.isalpha() for char in text)
+
+    @staticmethod
+    def is_all_uppercase(text: str) -> bool:
+        return bool(re.match(r'^[A-Z]+$', text.replace(" ", "")))
+
+    @staticmethod
+    def is_all_numeric(text: str) -> bool:
+        return bool(re.match(r'^\d+$', text))
+
+    @staticmethod
+    def convert_to_integer(text: str) -> int:
+        try:
+            return int(text)
+        except ValueError:
+            print(f"Cannot convert '{text}' to an integer.")
+            return None
+
+
