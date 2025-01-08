2780bb94ec2b58d203415bf58345dda705fda1ea
Ted Bouskill <618496+tedbouskill@users.noreply.github.com>
2025-01-08 01:06:56 -0600

    Moved functional prototypes to this file

--- repo_packages/cls_img_tools.py
+++ repo_packages/cls_img_tools.py
@@ -1,13 +1,48 @@
-import pytesseract
-import re
-import os
-import sqlite3
-import json
+import cv2
+import numpy as np
 
-from datetime import datetime, timedelta
-from PIL import Image, ImageOps
+from PIL import Image
 
 class ImageTools:
+    @staticmethod
+    def resize_image_opencv(image_path: str, new_width: int = 1200) -> np.ndarray:
+        # Read the image
+        img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+
+        # Get the original dimensions
+        original_height, original_width = img.shape[:2]
+
+        # Calculate the new dimensions
+        aspect_ratio = original_height / original_width
+        new_height = int(aspect_ratio * new_width)
+
+        # Resize the image
+        resized_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_LANCZOS4)
+
+        return resized_img, new_height
+
+    @staticmethod
+    def crop_image_opencv(image: np.ndarray, x: int, y: int, width: int, height: int) -> np.ndarray:
+        # Crop the image using array slicing
+        cropped_img = image[y:y+height, x:x+width]
+        return cropped_img
+
+    @staticmethod
+    def convert_non_white_to_black_opencv(image: np.ndarray, threshold=225) -> np.ndarray:
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Create a mask where pixels above the threshold are considered white
+        mask = gray > threshold
+
+        # Create an output image and set all pixels to black
+        output = np.zeros_like(image)
+
+        # Set the pixels that are above the threshold in the original image to white in the output image
+        output[mask] = [255, 255, 255]
+
+        return output
+
     @staticmethod
     def remove_everything_but_white(image, threshold=250):
         """
